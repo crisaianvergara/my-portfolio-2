2cd83db5423f9bbc640a0706dd5b596680e12b08
Cris-aian Vergara <crisaianvergara@gmail.com>
2024-07-21 19:45:40 +0800

    third commit

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ WORKDIR /my_portfolio
 COPY requirements.txt /my_portfolio/
 
 RUN pip install -r requirements.txt
+RUN pip install whitenoise
 
 COPY . /my_portfolio/
--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ["crisaianvergara.com", "localhost", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'main.apps.MainConfig',
     'projects.apps.ProjectsConfig',
     'blogs.apps.BlogsConfig',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
